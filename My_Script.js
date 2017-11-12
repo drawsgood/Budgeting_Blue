65768619e4ffe95d04e803a08a041fb8ccd86835
Michael Myers <mbmyers@gmail.com>
2017-11-12 14:37:56 -0600

    color changes

--- My_Script.js
+++ My_Script.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
 		var myVal = obj.startingValue;
 				$.each(obj.entries, function(key, value) {
 					finalNum -= Number(value.amount);
-					$('#list').append("<tr><td>$"+value.amount+"</td><td>"+value.category+"</td>");
+					$('#list').append("<tr class='myBorderColor'><td style='border: none;' class='myBGColor'>$"+value.amount+"</td><td style='border: none;' class='myBGColor'>"+value.category+"</td>");
 				});
 
-				$('#total').append("<h1 class='bg-primary text-center'>$"+myVal.fart.toFixed(2)+"</h1>");		
+				$('#total').append("<h1 class='myBGColor3 text-center'>$"+myVal.fart.toFixed(2)+"</h1>");		
 				console.log(myVal.fart.toFixed(2));		
 
 				console.log(finalNum.toFixed(2));
