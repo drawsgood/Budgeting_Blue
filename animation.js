67a3bea5528a2df304084dc368897d36298448c6
Michael Myers <mbmyers@gmail.com>
2017-11-13 17:38:19 -0600

    icons

--- animation.js
+++ animation.js
@@ -7,20 +7,21 @@ var myToggs = false;
 
 $("#openBTN").click(function(){
 
-	if (myToggs == false) {
-		$("#openBTN").addClass( "activatedStyling" );
-		$(".centerIcon").fadeOut("fast");
-		$("#openBTN").html("<i class='fa fa-close fa-large' aria-hidden='true'></i>");
-		myToggs = true;
-		console.log(myToggs);
-	} else {
-		$("#openBTN").removeClass( "activatedStyling" );
-		$(".centerIcon").fadeIn();
-		$("#openBTN").html("<i style='margin-top: 14px;' class='fa fa-plus fa-large' aria-hidden='true'></i>");
-		myToggs = false;
-		console.log(myToggs);
-	}
-
+//	if (myToggs == false) {
+//		$("#openBTN").addClass( "activatedStyling" );
+//		$(".centerIcon").fadeOut("fast");
+//		$("#openBTN").html("<i class='fa fa-close fa-large' aria-hidden='true'></i>");
+//		myToggs = true;
+//		console.log(myToggs);
+//	} else {
+//		$("#openBTN").removeClass( "activatedStyling" );
+//		$(".centerIcon").fadeIn();
+//		$("#openBTN").html("<i style='margin-top: 14px;' class='fa fa-plus fa-large' aria-hidden='true'></i>");
+//		myToggs = false;
+//		console.log(myToggs);
+//	}
+	$(".centerIcon").fadeToggle();
+    $(this).toggleClass('rotated activatedStyling');
 	$("#collapseMe").slideToggle("fast");
     $("#hideshowForm").slideToggle("fast");
 });
