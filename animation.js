0e77caabbf5fd10389a8aa206f0d9121b45da5fc
Michael Myers <mbmyers@gmail.com>
2017-11-13 16:26:49 -0600

    New styling

--- animation.js
+++ animation.js
@@ -2,7 +2,21 @@ $(document).ready(function(){
 
 $("#hideshowForm").hide();
 
+var myToggs = false;
+	console.log(myToggs);
+
 $("#openBTN").click(function(){
+
+	if (myToggs == false) {
+		$("#openBTN").html("<i class='fa fa-close fa-large' aria-hidden='true'></i>");
+		myToggs = true;
+		console.log(myToggs);
+	} else {
+		$("#openBTN").html("<i style='margin-top: 14px;' class='fa fa-plus fa-large' aria-hidden='true'></i>");
+		myToggs = false;
+		console.log(myToggs);
+	}
+
 	$("#collapseMe").slideToggle("fast");
     $("#hideshowForm").slideToggle("fast");
 });
